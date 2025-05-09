18413ca4412da8d947f277966b5512267b83f6da
Azure Devops Admin <faizanansari1974@outlook.com>
2025-05-09 16:34:20 +0000

    Intial Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files (HTML, CSS)
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+    console.log(req)
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
